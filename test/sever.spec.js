a0feaafaeeb26cdee41212f9fa26e6e1dbff0f97
Hugh Morrison <hmorri32@gmail.com>
2017-05-15 14:00:07 -0600

    testing setup, eslint

---
+++ test/sever.spec.js
@@ -0,0 +1,17 @@
+
+process.env.NODE_ENV = 'test';
+
+const chai     = require('chai');
+const should   = chai.should();
+const chaiHttp = require('chai-http');
+// const server   = require('../server');
+
+chai.use(chaiHttp);
+
+describe('testing stuff', () => {
+  it('shouldnt explode', () => {
+    let coolGuy = 'radical'
+
+    coolGuy.should.equal('radical')
+  });
+});
