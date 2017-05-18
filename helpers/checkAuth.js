20ea1926f07faa07dcb360b1e3701dfb3d7fcf9c
Hugh Morrison <hmorri32@gmail.com>
2017-05-17 20:36:18 -0600

    implement JWT

---
+++ helpers/checkAuth.js
@@ -0,0 +1,23 @@
+exports.checkAuth = (request, response, next) => {
+  const token = request.body.token ||
+                request.param('token') ||
+                request.headers['authorization'];
+  if (token) {
+    jwt.verify(token, app.get('secretKey'), (error, decoded) => {
+      if (error) {
+        return response.status(403).send({
+          success: false,
+          message: 'Invalid authorization token.'
+        });
+      } else {
+        request.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return response.status(403).send({
+      success: false,
+      message: 'you must be authorized to hit this yung endpoint'
+    });
+  }
+};
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express    = require('express');
-const app        = express();
-const path       = require('path');
-const bodyParser = require('body-parser');
-const favicon    = require('serve-favicon');
-const routes     = require('./routes/index');
-const error      = require('./helpers/error');
-// const session       = require('express-session');
-// const cookieParser  = require('cookie-parser');
+const express       = require('express');
+const app           = express();
+const path          = require('path');
+const bodyParser    = require('body-parser');
+const favicon       = require('serve-favicon');
+const routes        = require('./routes/index');
+const error         = require('./helpers/error');
+const config        = require('dotenv').config().parsed;
+const jwt           = require('jsonwebtoken');
+const { checkAuth } = require('./helpers/checkAuth');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.set('port', process.env.PORT || 3000);
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('secretKey', config.CLIENT_SECRET);
+
+if (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD) {
+  throw 'Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file';
+}
 
 app.use('/', routes);
 
@@ -26,8 +32,8 @@ if (app.get('env') === 'development') {
   app.use(error.developmentErrors);
 }
 
-const server = app.listen(app.get('port'), () => {
-  console.log(`Magic happens on port ${server.address().port}`);
+app.listen(app.get('port'), () => {
+  console.log(`Magic happens on port ${app.get('port')}`);
 });
 
 module.exports = app;
