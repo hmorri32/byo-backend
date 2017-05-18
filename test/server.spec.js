2f55c347ccbddd84232400e88bd06b10b0633dfe
Hugh Morrison <hmorri32@gmail.com>
2017-05-17 18:20:18 -0600

    testing yung query param for sharks

--- test/server.spec.js
+++ test/server.spec.js
@@ -72,41 +72,47 @@ describe('server side testing', () => {
         chai.request(server)
         .get('/api/v1/sharks')
         .end((error, response) => {
+
+          const firstShark  = response.body[0];
+          const secondShark = response.body[1];
+
           response.should.have.status(200);
           response.should.be.json;
           response.body.should.be.a('array');
           response.body.length.should.equal(2);
-          response.body[0].should.have.property('id');
-          response.body[0].should.have.property('shark_id');
-          response.body[0].should.have.property('name');
-          response.body[0].should.have.property('tagIdNumber');
-          response.body[0].should.have.property('species');
-          response.body[0].should.have.property('gender');
-          response.body[0].should.have.property('stageOfLife');
-          response.body[0].should.have.property('length');
-          response.body[0].should.have.property('weight');
-          response.body[0].should.have.property('tagDate');
-          response.body[0].should.have.property('tagLocation');
-          response.body[0].should.have.property('description');
-          response.body[1].should.have.property('id');
-          response.body[1].should.have.property('shark_id');
-          response.body[1].should.have.property('name');
-          response.body[1].should.have.property('tagIdNumber');
-          response.body[1].should.have.property('species');
-          response.body[1].should.have.property('gender');
-          response.body[1].should.have.property('stageOfLife');
-          response.body[1].should.have.property('length');
-          response.body[1].should.have.property('weight');
-          response.body[1].should.have.property('tagDate');
-          response.body[1].should.have.property('tagLocation');
-          response.body[1].should.have.property('description');
+          
+          firstShark.should.have.property('id');
+          firstShark.should.have.property('shark_id');
+          firstShark.should.have.property('name');
+          firstShark.should.have.property('tagIdNumber');
+          firstShark.should.have.property('species');
+          firstShark.should.have.property('gender');
+          firstShark.should.have.property('stageOfLife');
+          firstShark.should.have.property('length');
+          firstShark.should.have.property('weight');
+          firstShark.should.have.property('tagDate');
+          firstShark.should.have.property('tagLocation');
+          firstShark.should.have.property('description');
 
-          response.body[0].id.should.equal(1);
-          response.body[1].id.should.equal(2);
+          secondShark.should.have.property('id');
+          secondShark.should.have.property('shark_id');
+          secondShark.should.have.property('name');
+          secondShark.should.have.property('tagIdNumber');
+          secondShark.should.have.property('species');
+          secondShark.should.have.property('gender');
+          secondShark.should.have.property('stageOfLife');
+          secondShark.should.have.property('length');
+          secondShark.should.have.property('weight');
+          secondShark.should.have.property('tagDate');
+          secondShark.should.have.property('tagLocation');
+          secondShark.should.have.property('description');
+
+          firstShark.id.should.equal(1);
+          secondShark.id.should.equal(2);
           done();
         });
       });
-      
+
       it('should return 404 for non existent route and render HTML error', (done) => {
         chai.request(server)
         .get('/api/v1/sharkzzzz')
@@ -114,6 +120,38 @@ describe('server side testing', () => {
           response.should.have.status(404);
           response.should.be.html;
           response.res.text.should.include('Route Not Found!');
+          done();
+        });
+      });
+
+      it('should get sharks using that sweet sweeet query string', (done) => {
+        chai.request(server)
+        .get('/api/v1/sharks?species=Jaguar shark')
+        .end((error, response) => {
+          const yungJaguarShark = response.body[0];
+
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+
+          yungJaguarShark.should.have.property('id');
+          yungJaguarShark.should.have.property('shark_id');
+          yungJaguarShark.should.have.property('name');
+          yungJaguarShark.should.have.property('tagIdNumber');
+          yungJaguarShark.should.have.property('species');
+          yungJaguarShark.should.have.property('gender');
+          yungJaguarShark.should.have.property('stageOfLife');
+          yungJaguarShark.should.have.property('length');
+          yungJaguarShark.should.have.property('weight');
+          yungJaguarShark.should.have.property('tagDate');
+          yungJaguarShark.should.have.property('tagLocation');
+          yungJaguarShark.should.have.property('description');
+
+          yungJaguarShark.id.should.equal(1);
+          yungJaguarShark.shark_id.should.equal(12);
+          yungJaguarShark.name.should.equal('Alistair Hennessey');
+
           done();
         });
       });
