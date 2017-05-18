374fb88fc7d8d3c353472554b2badf084b761123
Hugh Morrison <hmorri32@gmail.com>
2017-05-18 16:49:49 -0600

    circle CI env variables

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('secretKey', config.CLIENT_SECRET || process.env.CLIENT_SECRET);
+app.set('secretKey', process.env.CLIENT_SECRET || config.CLIENT_SECRET);
 
 if (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD) {
   throw 'Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file';
