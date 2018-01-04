d609fa254e39d112d60281f5ac7c814ea085edb6
Hugh Morrison <hmorri32@gmail.com>
2018-01-04 10:04:17 -0700

    normalize migration

--- db/migrations/20170515164423_initial.js
+++ db/migrations/20170515164423_initial.js
@@ -3,42 +3,39 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('sharks', function(table) {
-      table.increments('id').primary();
-      table.integer('shark_id');
-      table.string('name');
-      table.string('tagIdNumber');
-      table.string('species');
-      table.string('gender');
-      table.string('stageOfLife');
-      table.string('length');
-      table.string('weight');
-      table.string('tagDate');
-      table.string('tagLocation');
-      table.string('description');
+    knex.schema.createTable("sharks", function(table) {
+      table.increments("id").primary();
+      table.integer("shark_id");
+      table.string("name");
+      table.string("tagIdNumber");
+      table.string("species");
+      table.string("gender");
+      table.string("stageOfLife");
+      table.string("length");
+      table.string("weight");
+      table.string("tagDate");
+      table.string("tagLocation");
+      table.string("description");
       table.timestamps(true, true);
     }),
 
-    knex.schema.createTable('pings', function(table) {
-      table.increments('id').primary();
-      table.integer('key');
-      table.string('shark_id');
-      table.string('ping_id');
-      table.string('datetime');
-      table.string('tz_datetime');
-      table.string('latitude');
-      table.string('longitude');
+    knex.schema.createTable("pings", function(table) {
+      table.increments("id").primary();
+      table.integer("key");
+      table.string("shark_id");
+      table.string("ping_id");
+      table.string("datetime");
+      table.string("tz_datetime");
+      table.string("latitude");
+      table.string("longitude");
       table.timestamps(true, true);
     })
   ]);
 };
 
-
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('pings'),
-    knex.schema.dropTable('sharks')
+    knex.schema.dropTable("pings"),
+    knex.schema.dropTable("sharks")
   ]);
 };
-
-
