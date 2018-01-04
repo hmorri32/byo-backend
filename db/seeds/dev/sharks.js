87c85c3e5fbe1c3d9d89204a86e2421888f5fea5
Hugh Morrison <hmorri32@gmail.com>
2018-01-04 10:04:25 -0700

    prettier sharks

--- db/seeds/dev/sharks.js
+++ db/seeds/dev/sharks.js
@@ -1,37 +1,52 @@
 // knex seed:run
 
-const path     = require('path');
-const seedFile = require('knex-seed-file');
-
+const path = require("path");
+const seedFile = require("knex-seed-file");
 
 exports.seed = function(knex, Promise) {
-  return Promise.all([knex('pings').del(), knex('sharks').del()])
-  .then(() => seedFile(knex, path.resolve('data/sharks.csv'), 'sharks', [
-    'shark_id',
-    'name',
-    'tagIdNumber',
-    'species',
-    'gender',
-    'stageOfLife',
-    'length',
-    'weight',
-    'tagDate',
-    'tagLocation',
-    'description'
-  ], {
-    columnSeparator: ',',
-    ignoreFirstLine: true
-  }))
-  .then(() => seedFile(knex, path.resolve('data/pings.csv'), 'pings', [
-    'key',
-    'shark_id',
-    'ping_id',
-    'datetime',
-    'tz_datetime',
-    'latitude',
-    'longitude',
-  ], {
-    columnSeparator: ',',
-    ignoreFirstLine: true
-  }));
+  return Promise.all([knex("pings").del(), knex("sharks").del()])
+    .then(() =>
+      seedFile(
+        knex,
+        path.resolve("data/sharks.csv"),
+        "sharks",
+        [
+          "shark_id",
+          "name",
+          "tagIdNumber",
+          "species",
+          "gender",
+          "stageOfLife",
+          "length",
+          "weight",
+          "tagDate",
+          "tagLocation",
+          "description"
+        ],
+        {
+          columnSeparator: ",",
+          ignoreFirstLine: true
+        }
+      )
+    )
+    .then(() =>
+      seedFile(
+        knex,
+        path.resolve("data/pings.csv"),
+        "pings",
+        [
+          "key",
+          "shark_id",
+          "ping_id",
+          "datetime",
+          "tz_datetime",
+          "latitude",
+          "longitude"
+        ],
+        {
+          columnSeparator: ",",
+          ignoreFirstLine: true
+        }
+      )
+    );
 };
