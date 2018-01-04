7ecba74c1db5ac78d340bbf998c8bb3a95af6f7b
Hugh Morrison <hmorri32@gmail.com>
2018-01-04 10:05:09 -0700

    cosmetic changes to quotes in parsedata

--- helpers/parseData.js
+++ helpers/parseData.js
@@ -1,14 +1,14 @@
-const json2csv  = require('json2csv');
-const fs        = require('fs');
-const sharkData = require('../data/sharkData.js');
-const pingData  = require('../data/pingData.js');
+const json2csv  = require("json2csv");
+const fs        = require("fs");
+const sharkData = require("../data/sharkData.js");
+const pingData  = require("../data/pingData.js");
 const sharks    = json2csv({ data: sharkData });
 const pings     = json2csv({ data: pingData });
- 
-fs.writeFile('../data/sharks.csv', sharks, function(err) {
+
+fs.writeFile("../data/sharks.csv", sharks, function(err) {
   if (err) throw err;
 });
 
-fs.writeFile('../data/pings.csv', pings, function(err) {
-  if(err) throw err;
+fs.writeFile("../data/pings.csv", pings, function(err) {
+  if (err) throw err;
 });
